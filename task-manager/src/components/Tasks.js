bb0474e1b0e4cc778607aa7ccf478df18396f66f
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-30 17:21:00 +0530

    changes

--- task-manager/src/components/Tasks.js
+++ task-manager/src/components/Tasks.js
@@ -1,28 +1,36 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2.30pm",
-    reminder: true,
-  },
-
-  {
-    id: 2,
-    text: "Office Appointment",
-    day: "Feb 5th at 3.30pm",
-    reminder: true,
-  },
-
-  {
-    id: 3,
-    text: "Shopping",
-    day: "Feb 5th at 4.30pm",
-    reminder: false,
-  },
-];
+import { useState } from "react";
 
 function Tasks() {
-  return <div></div>;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2.30pm",
+      reminder: true,
+    },
+
+    {
+      id: 2,
+      text: "Office Appointment",
+      day: "Feb 5th at 3.30pm",
+      reminder: true,
+    },
+
+    {
+      id: 3,
+      text: "Shopping",
+      day: "Feb 5th at 4.30pm",
+      reminder: false,
+    },
+  ]);
+  
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
 }
 
 export default Tasks;
