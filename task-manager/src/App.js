6615956179b18faaa305abac0f25717b3306b4ad
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-30 18:56:45 +0530

    changes

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,32 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2.30pm",
-      reminder: true,
-    },
-
-    {
-      id: 2,
-      text: "Office Appointment",
-      day: "Feb 5th at 3.30pm",
-      reminder: true,
-    },
-
-    {
-      id: 3,
-      text: "Shopping",
-      day: "Feb 5th at 4.30pm",
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   //add tasks
   const addTask = (task) => {
