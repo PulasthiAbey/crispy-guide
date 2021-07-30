4067be25735956c62b07a7f5cf0510eba42c673b
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-30 17:11:08 +0530

    changes

--- task-manager/src/components/Tasks.js
+++ task-manager/src/components/Tasks.js
@@ -1,9 +1,28 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2.30pm",
+    reminder: true,
+  },
+
+  {
+    id: 2,
+    text: "Office Appointment",
+    day: "Feb 5th at 3.30pm",
+    reminder: true,
+  },
+
+  {
+    id: 3,
+    text: "Shopping",
+    day: "Feb 5th at 4.30pm",
+    reminder: false,
+  },
+];
+
 function Tasks() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div></div>;
 }
 
-export default Tasks
+export default Tasks;
