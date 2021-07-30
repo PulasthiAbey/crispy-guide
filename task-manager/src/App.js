928ae9c414936b91c2f0edcb92e9f6ea4b7814e3
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-30 17:31:57 +0530

    changes

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2.30pm",
+      reminder: true,
+    },
+
+    {
+      id: 2,
+      text: "Office Appointment",
+      day: "Feb 5th at 3.30pm",
+      reminder: true,
+    },
+
+    {
+      id: 3,
+      text: "Shopping",
+      day: "Feb 5th at 4.30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="App">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- task-manager/src/components/Tasks.js
+++ task-manager/src/components/Tasks.js
@@ -1,33 +1,10 @@
-import { useState } from "react";
+import Task from "./Task";
 
-function Tasks() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2.30pm",
-      reminder: true,
-    },
-
-    {
-      id: 2,
-      text: "Office Appointment",
-      day: "Feb 5th at 3.30pm",
-      reminder: true,
-    },
-
-    {
-      id: 3,
-      text: "Shopping",
-      day: "Feb 5th at 4.30pm",
-      reminder: false,
-    },
-  ]);
-  
+function Tasks(props) {
   return (
     <>
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+      {props.tasks.map((task) => (
+        <Task key={props.task.id} task={props.task} />
       ))}
     </>
   );
